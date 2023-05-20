c7d882828371d73bbf90c523285ea368329b26b8
JavaScalaDeveloper <Hjh544789628>
2023-05-20 12:29:14 +0800

    添加所有class的包名

---
+++ change/datastructure/ListNode.java
@@ -0,0 +1,22 @@
+package change.datastructure;
+
+
+/**
+ * 链表
+ */
+public class ListNode {
+    public int val;
+    public ListNode next = null;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode() {
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ change/datastructure/TreeNode.java
@@ -0,0 +1,24 @@
+package change.datastructure;
+
+/**
+ * 二叉树
+ */
+//@Data
+public class TreeNode {
+    public int val;
+    public TreeNode left = null;
+    public TreeNode right = null;
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode() {
+    }
+}
